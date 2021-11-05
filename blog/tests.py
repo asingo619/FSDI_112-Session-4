99579c1ab8087a2908d16783cb229e54cd6b8c22
asingo619 <andrew.singo@gmail.com>
2021-11-05 00:31:19 -0700

    second commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,4 @@
-from django.http import response
+# from django.http import response
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -8,7 +8,7 @@ from .models import Post
 
 class BlogTests(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.creat_user(
+        self.user = get_user_model().objects.create_user(
             username = 'test',
             email = 'test@test.com',
             password = 'secret'
@@ -31,7 +31,7 @@ class BlogTests(TestCase):
     def test_post_list_view(self):
         response = self.client.get("/")  
         self.assertEqual(response.status_code, 200)
-        self.assertcontains(response, "body.html")
+        self.assertContains(response, "body.html")
         self.assertTemplateUsed(response, "home.html")
         self.assertTemplateUsed(response, "base.html")
 
@@ -44,4 +44,12 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, "post_detal.html")
         self.assertTemplateUsed(response, "base.html")
 
-    # def 
+    def test_reverse_lookup(self):
+        post_list_response = self.client.get(reverse("home"))
+        post_detail_response = self.client.get(reverse("post_detail", args=["1"]))
+        self.assertEqual(post_list_response.status_code, 200)
+        self.assertEqual(post_detail_response.status_code, 200)
+        self.assertContains(post_list_response, "A title")
+        self.assertTemplateUsed(post_list_response,"home.html")
+        self.assertContains(post_detail_response, "A title")
+        self.assertTemplateUsed(post_detail_response,"post_detail.html")
