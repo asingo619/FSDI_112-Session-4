792a76235451591709483c9d5b503f1e5fdde83e
asingo619 <andrew.singo@gmail.com>
2021-11-05 01:36:06 -0700

    CR

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -10,3 +11,14 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_anem = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "body", "author"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
