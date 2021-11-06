101b39544e0364fda9b0379fc05e51248c7a067a
asingo619 <andrew.singo@gmail.com>
2021-11-06 12:51:42 -0700

    Added login and signup

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+
+from blog.views import BlogDeleteView
 from .views import(
      BlogListView, 
      BlogDetailView,
      BlogCreateView,
-     BlogUpdateView
+     BlogUpdateView,
+     BlogDeleteView
 )
 
 
@@ -12,4 +15,5 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name="post_new"),
     path('post/<int:pk>/edit', BlogUpdateView.as_view(), name="post_edit"),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name="post_delete"),
 ]
